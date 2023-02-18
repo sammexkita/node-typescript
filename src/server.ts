e18472ee4afedbb19b49720ebcb104246f5b983a
Kita Igniter <smtecsystem.469@gmail.com>
2023-02-18 17:08:34 -0300

    feat(api)!: adding category route

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({"message": "Hello Worlds!"});
-});
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("App is running on port 3333 🚀");
