d66b8ebdd191275d33f1bc68275c4a54853e6069
Kita Igniter <smtecsystem.469@gmail.com>
2023-02-18 16:46:38 -0300

    chore!: adding debugging settings in application

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
   return res.json({"message": "Hello Worlds!"});
 });
 
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
 app.listen(3333, () => {
   console.log("App is running on port 3333 🚀");
 });
