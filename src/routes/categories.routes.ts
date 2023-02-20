db394b89d0c2a674a5511b8c64a7ae5abe3d840c
Kita Igniter <smtecsystem.469@gmail.com>
2023-02-20 20:37:58 -0300

    feat(api)!: Adding Category Repositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,13 @@
 import { Router } from "express";
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories:Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return res.status(201).json({ category });
+  categoriesRepository.create({ name, description });
+  return res.status(201).send();
 });
 
 export { categoriesRoutes };
