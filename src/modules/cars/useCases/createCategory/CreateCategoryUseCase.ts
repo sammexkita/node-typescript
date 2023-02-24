52bace844eac534b1e0cdca06cd86e8040891e4d
Kita Igniter <smtecsystem.469@gmail.com>
2023-02-23 21:12:28 -0300

    feat!: Creating Category Use Case

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,12 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ name, description }: IRequest):void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
@@ -18,4 +19,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
