4f0653af5b850caf4011245c31ca429e13d19dd2
Kita Igniter <smtecsystem.469@gmail.com>
2023-02-27 20:42:24 -0300

    feat!: Creating use case to import categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 import multer from "multer";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
@@ -18,9 +19,7 @@ categoriesRoutes.get("/", (req, res) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
-  const { file } = req;
-  console.log(file);
-  return res.send();
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
